f90a375ae0dbe0a91d12dc5698017fb6bc2ab53b
suzal-29 <suzal.k@weboccult.com>
2025-02-17 14:11:55 +0530

    guess number optimized

--- guess_num.py
+++ guess_num.py
@@ -21,6 +21,17 @@ while True:
     while True:
         guess_num=int(input())
 
+        if attempts==5:
+            print(f"you have taken many attempts. you are having only 5 attempts max to get the number right.")
+            choice=int(input("please enter 1 for play again or 2 for exit"))
+            if choice==1:
+                print(f"you have taken {attempts} attempts before guessing the right number")
+                break
+            else:
+                print(f"you have taken {attempts} attempts before guessing the right number")
+                exit()
+
+
         if guess_num not in range(1,10):
             raise InvalidInputError("please enter the number in range 1 to 10 only")
 
